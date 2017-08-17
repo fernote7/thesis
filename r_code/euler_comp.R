590917b5ca5ec8ec5ffd78d62db184f04779d31e
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-08-17 18:34:09 -0300

    .

---
+++ r_code/euler_comp.R
@@ -0,0 +1,19 @@
+euler = matrix(data= c(100.00000,100.00000, 
+59.72682, 57.63416,
+39.72776, 37.70413, 
+29.79651, 28.32850,
+24.86481, 23.91795, 
+22.41579, 21.84311, 
+21.19965, 20.86705, 
+20.59573, 20.40788, 
+20.29583, 20.19188, 
+20.14690, 20.09027, 
+20.07295, 20.04246), ncol = 2, byrow = TRUE) 
+
+plot(euler[,1], lty = 1, pch = 1, type='b', axes=F, xlab = 'time in s', ylab = 'Temperature', col='blue', cex=0.5)
+lines(euler[,2], lty = 2, pch = 20, type='b', col='red', cex = 0.5)
+axis(side = 1, at = c(1,3,5,7,9,11), labels = c("0","20","40","60","80","100"))
+axis(side = 2, las = 1)
+box()
+leg.txt = c("analytical", "euler")
+legend("topright", leg.txt,col=c("blue", "red"),lwd=c(1,1),lty=c(1,2), pch = c(1,20),seg.len=2,bty="n", cex = 0.75)
--- r_code/smile.R
+++ r_code/smile.R
@@ -1,21 +1,18 @@
-x = c()
-x[1] <- (-10)^2
+x = c(100, 81, 64, 49, 36, 25, 16,  9,  4,  1,  
+      0,  1,  4,  9, 16, 25, 36, 49, 64, 81, 100)
+cex = 0.4
 
-for (i in 2:21){
-  
-  x[i] <- (i-11)^2
-  
-  
-}
-
-print(x)
 plot(x, type='l', axes=FALSE, xlim = c(-3,28), ylim = c(-90,170), xlab = "Spot Price",
-     ylab = "implied Volatility")
+     ylab = "Implied Volatility")
 segments(x0=11,y0=-100,x1=11,y1=0,lty=2, col = 'red')
-mtext("ATM", side = 1, col = 'red', cex = 0.8, lwd=10)
+mtext("At-the-money Calls", side = 1, col = 'red', cex = 0.6, lwd=10, at=11)
 box()
-arrows(x0=10,y0=-20,x1=7,y1=-20, col = 'black', length = 0.1)
-arrows(x0=15,y0=-20,x1=12,y1=-20, col = 'black', length = 0.1, code = 1)
-text("Out of the money Calls", x = 6.5, y = -30, col = 'black', cex = 0.6)
-text("In the money Calls", x = 15, y = -30, col = 'black', cex = 0.6)
+arrows(x0=10,y0=-17,x1=7,y1=-17, col = 'black', length = 0.07)
+arrows(x0=15,y0=-17,x1=12,y1=-17, col = 'black', length = 0.07, code = 1)
+text("Out-of-the-money Calls", x = 7, y = -33, col = 'black', cex = cex)
+text("In-the-money Calls", x = 15, y = -33, col = 'black', cex = cex)
 
+text("Volatility increases as the option becomes \n increasingly in-the-money or out-of-the-money", x = 11.5, y = 75, col = 'black', cex = cex)
+arrows(x0=9,y0=55,x1=7,y1=35, col = 'red', length = 0.07)
+arrows(x0=13,y0=55,x1=15,y1=35, col = 'red', length = 0.07, code = 2)
+points(x=11, y = 0, pch = 19, col = 'black', cex=0.5)
