884790346c8ae6adad829181189548b6edc9598f
fernote7 <fernote7@gmail.com>
2017-09-10 20:26:09 -0300

    .

--- r_code/parametrization.R
+++ r_code/parametrization.R
@@ -1,8 +1,8 @@
 source("./r_code/parameters.R")
-options("scipen"=100, "digits"=4)
+options("scipen"=100, "digits"=2)
 a <- mget(c("dt","k","r","rho","S","sigma","t","tau","theta","v","X"))
 # print(a)
-aa <- data.frame(variables = names(a), values = unlist(a), row.names = NULL)
+aa <- data.frame(Variables = names(a), Values = unlist(a), row.names = NULL)
 print(knitr::kable(aa))
 
 
