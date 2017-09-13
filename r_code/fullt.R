57658e7d23acde55b82dd41158e50d27a8817859
Fernando Ormonde Teixeira <fernando.teixeira@fgv.br>
2017-09-13 18:25:52 -0300

    .

---
+++ r_code/fullt.R
@@ -0,0 +1,11 @@
+mat <- matrix(c("Reflection", "$\\mid V \\mid$", "$\\mid V \\mid$", "$\\mid V \\mid$",
+                "Partial Truncation", "$V$", "$V$", "$V^+$",
+                "Full Truncation", "$V$", "$V^+$", "$V^+$"), nrow = 3, ncol = 4, byrow = TRUE)
+colnames(mat) <- c("Scheme", "$f_1(V_{t})$", "$f_2(V_{t})$", "$f_3(V_{t})$")
+#align(mat) <- c("|c","c","c|")
+#print(mat, sanitize.text.function = function(x) {x})
+
+print(knitr::kable(mat, caption = 'Truncation schemes', align = c('l','r', 'r', 'r'),
+                   longtable = TRUE, booktabs = TRUE, escape = FALSE))
+
+
